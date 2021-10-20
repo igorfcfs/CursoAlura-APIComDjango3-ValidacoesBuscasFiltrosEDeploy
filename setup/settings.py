c4a5bcf1c7ac0e8d693e6a9cc250aa7bd23770d5
igorfcfs <igorfcfs@gmail.com>
2021-10-20 17:46:57 -0300

    Parte5

--- setup/settings.py
+++ setup/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
 
@@ -126,3 +128,5 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 25
 }
+
+django_heroku.settings(locals())
